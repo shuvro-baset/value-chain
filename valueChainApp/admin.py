955af3f3f4dd69a6b6875c0b111a794497afe822
shuvro-baset <md.abdul.baset75@gmail.com>
2024-01-25 00:28:14 +0600

    chore: add all model in admin

--- valueChainApp/admin.py
+++ valueChainApp/admin.py
@@ -1,3 +1,58 @@
 from django.contrib import admin
+from .models import *
+
 
 # Register your models here.
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ['product_name', 'product_group', 'uom', 'qty', 'rate', 'stock_qty', 'stock_rate', 'avg_rate']
+
+
+@admin.register(ProductIssue)
+class ProductIssueAdmin(admin.ModelAdmin):
+    list_display = ['creator', 'date', 'description', 'total_qty', 'product', 'status', 'production_cost', 'unit_price']
+
+
+@admin.register(RawMaterials)
+class RawMaterialsAdmin(admin.ModelAdmin):
+    list_display = ['creator', 'date', 'description', 'product', 'product_issue']
+
+
+@admin.register(PurchaseOrder)
+class PurchaseOrderAdmin(admin.ModelAdmin):
+    list_display = ['raw_materials', 'creator', 'date', 'description', 'product', 'product_issue', 'total_qty', 'total']
+
+
+@admin.register(PurchaseRecipt)
+class PurchaseReciptAdmin(admin.ModelAdmin):
+    list_display = ['purchase_order', 'creator', 'date', 'description', 'product', 'product_issue', 'total_qty', 'total']
+
+
+@admin.register(CostType)
+class CostTypeAdmin(admin.ModelAdmin):
+    list_display = ['name', 'total']
+
+
+@admin.register(ProductionCost)
+class ProductionCostAdmin(admin.ModelAdmin):
+    list_display = ['creator', 'date', 'cost_type', 'product_issue', 'total']
+
+
+@admin.register(OthersCost)
+class OthersCostAdmin(admin.ModelAdmin):
+    list_display = ['creator', 'date', 'cost_type', 'product_issue', 'total']
+
+
+@admin.register(StockEntry)
+class StockEntryAdmin(admin.ModelAdmin):
+    list_display = ['creator', 'date', 'product', 'product_issue', 'total_qty', 'total']
+
+
+@admin.register(SalesOrder)
+class SalesOrderAdmin(admin.ModelAdmin):
+    list_display = ['creator', 'date', 'description', 'product', 'total_qty', 'total']
+
+
+@admin.register(DeliveryChallan)
+class DeliveryChallanAdmin(admin.ModelAdmin):
+    list_display = ['sales_order', 'creator', 'date', 'description', 'product', 'total_qty', 'total']
